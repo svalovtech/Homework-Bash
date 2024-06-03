0ef3a15c4fd119a7eca41fb2c22273eed8b14146
svaloctech <svalovtech@gmail.com>
2024-06-03 20:37:05 +0000

    add function one

--- homework10/function.sh
+++ homework10/function.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
 
+function 1st() {
+
+  useradd tim
+  useradd brad
+  useradd ann
+
+  mkdir kaizen
+  mkdir slava
+
+  touch hello
+  touch world
+}
+1st
+
+function 2nd() {
+
+  yum install -y wget httpd tree git
+}
+2nd
+
+echo -e "1-function 1st\n2-function 2nd"
+
+read -p "Please pick function: " function
+
+ if [ $function -eq 1 ]
+  then
+1st
+elif [ $function -eq 2 ]
+  then
+2nd
+fi
 
